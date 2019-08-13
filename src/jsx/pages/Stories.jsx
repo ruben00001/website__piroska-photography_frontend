8c312a1c1b3193dada5202f03bbe0071aacd800d
unknown <a.ruben00001@gmail.com>
2019-08-13 16:47:50 +0100

    stories animations

--- src/jsx/pages/Stories.jsx
+++ src/jsx/pages/Stories.jsx
@@ -29,10 +29,9 @@ class StoriesX extends Component {
             imageStyles: [],
             counterDuration: 20,
             loadingWidgetOut: false,
-            stopLoader: false
+            stopLoader: false,
+            titlesIn: false
         }
-
-        this.setFloatAndPaddingTop = this.setFloatAndPaddingTop.bind(this);
     }
 
     homeURL = strapiAPI;
@@ -61,24 +60,13 @@ class StoriesX extends Component {
                     })
                 })
             })
-            .then(_ => {
-                this.determineFloat();
-                this.setFloatAndPaddingTop();
-            })
     }
 
-    imagesOnLoad = ({ target: img }) => {
-        const imageDimension = img.offsetWidth / img.offsetHeight;
-
+    imagesOnLoad = () => {
         this.setState({
             numImagesLoaded: this.state.numImagesLoaded + 1,
-            imageDimensions: [...this.state.imageDimensions, imageDimension]
         }, _ => {
             if (this.state.numImagesLoaded === this.state.stories.length) {
-                this.imgWidthMargin();
-                this.setState({
-                    counterDuration: 5
-                })
                 setTimeout(() => {
                     this.setState({
                         imagesLoaded: true
@@ -88,168 +76,20 @@ class StoriesX extends Component {
                     this.setState({
                         stopLoader: true
                     })
-                }, 900)
+                }, 900);
                 setTimeout(_ => {
                     this.setState({
                         loadingWidgetOut: true
                     })
-                }, 1300)
-            }
-        })
-    }
-
-    onImageLoad = () => {
-        this.setState({
-            imageLoaded: true,
-        })
-        setTimeout(_ => {
-            this.setState({
-                stopLoader: true
-            })
-        }, 900)
-        setTimeout(_ => {
-            this.setState({
-                loadingWidgetOut: true
-            })
-        }, 1300)
-        setTimeout(_ => {
-            this.setState({
-                showPage: true
-            })
-        }, 1850)
-        setTimeout(_ => {
-            this.setState({
-                scalePage: true,
-            })
-        }, 2050)
-    }
-
-    determineFloat = () => {
-        let floats = [];
-        floats.push(Math.round(Math.random()))
-        for (let i = 1; i < this.state.stories.length; i++) {
-            if (floats[i - 1] === 0 && Math.random() < 6 / 11) {
-                floats.push(1)
-            } else if (floats[i - 1] === 1) {
-                floats.push(2)
-            } else {
-                floats.push(0)
-            }
-        }
-        this.setState({
-            floats: floats
-        })
-    }
-
-    rdmNum = (x, y) => {
-        return x + Math.random() * (y - x);
-    }
-
-    setFloatAndPaddingTop = () => {
-        let floatStyles = [];
-        this.state.stories.forEach((story, i) => {
-            let style = null;
-            let paddingTop = 0;
-            if (this.state.windowWidth > 1200) paddingTop = this.rdmNum(110, 180);
-            if (this.state.windowWidth <= 1200 && this.state.windowWidth > 960) paddingTop = this.rdmNum(80, 150);
-            if (this.state.windowWidth <= 960 && this.state.windowWidth > 800) paddingTop = this.rdmNum(60, 130);
-            if (this.state.windowWidth > 600 && this.state.windowWidth <= 800) paddingTop = this.rdmNum(50, 90);
-            if (this.state.windowWidth <= 600) paddingTop = this.rdmNum(30, 90);
-
-            if (this.state.windowWidth > 800) {
-                if (this.state.floats[i] === 1) {
-                    if (!this.state.storyPaddingTop && Math.random() < 1 / 2) {
-                        paddingTop += 70;
-                        this.setState({
-                            storyPaddingTop: true
-                        })
-                    } else {
-                        paddingTop = 60;
-                    }
-                    style = {
-                        float: 'left',
-                        width: '50%',
-                        paddingTop: paddingTop
-                    };
-
-                } else if (this.state.floats[i] === 2) {
-                    if (!this.state.storyPaddingTop) {
-                        paddingTop += 70;
-                    } else {
-                        paddingTop = 60;
-                    }
+                }, 1300);
+                setTimeout(_ => {
                     this.setState({
-                        storyPaddingTop: false
+                        titlesIn: true
                     })
-                    style = {
-                        float: 'right',
-                        width: '50%',
-                        paddingTop: paddingTop
-                    }
-
-                } else {
-                    style = {
-                        width: '100%',
-                        paddingTop: paddingTop
-                    }
-                }
-
-            } else {
-                style = {
-                    width: '100%',
-                    paddingTop: paddingTop
-                }
+                }, 2000);
             }
-
-            floatStyles.push(style)
-        })
-        this.setState({
-            containerStyles: floatStyles
         })
     }
-
-    imgWidthMargin = () => {
-        let styles = [];
-        this.state.stories.forEach((story, i) => {
-            let width = 0;
-            let multiplier = 1.5;
-            let dimension = this.state.imageDimensions[i];
-
-            if (this.state.windowWidth > 600 && this.state.windowWidth <= 800) multiplier = 1.5;
-            else if (this.state.windowWidth <= 600) multiplier = 2.5;
-            else if (this.state.floats[i]) multiplier = 3;
-
-            const calcStyle = (a, b) => {
-                width = dimension * multiplier * this.rdmNum(a, b);
-                return { width: `${width}%`, marginLeft: `${this.rdmNum(width, 90) - width}%` }
-            }
-
-            if (dimension < 0.65) {
-                styles.push(calcStyle(29, 32))
-            }
-            if ((dimension >= .65 && dimension < .9)) {
-                styles.push(calcStyle(25, 33))
-            }
-            if (dimension >= .9 && dimension < 1.1) {
-                styles.push(calcStyle(20, 33))
-            }
-            if (dimension >= 1.1 && dimension < 1.3) {
-                styles.push(calcStyle(17, 28))
-            }
-            if (dimension >= 1.3 && dimension < 1.6) {
-                styles.push(calcStyle(15, 23))
-            }
-            if (dimension >= 1.6) {
-                styles.push(calcStyle(14, 18))
-            }
-        })
-        this.setState({
-            imageStyles: styles
-        })
-    }
-
-
-
 
     showStory = (e) => {
         this.setState({
@@ -268,51 +108,61 @@ class StoriesX extends Component {
             <React.Fragment>
                 <Route exact path="/stories" render={() =>
                     <div className='stories-page'>
-                        {/* <div className='loading-screen'>
+                        <div className='loading-screen'>
                             <div className='x' >
                                 <Spring
-                                    from={{ opacity: 0 }}
-                                    to={{ opacity: !this.state.loadingWidgetOut ? 1 : 0 }}
+                                    from={{ opacity: 0, transform: 'translateY(0px)' }}
+                                    to={{
+                                        opacity: 1,
+                                        transform: !this.state.loadingWidgetOut ? 'translateY(0px)' : 'translateY(-100px)'
+                                    }}
                                     config={config.slow}
                                 >
                                     {props =>
                                         <div style={props}>
-                                            {!this.state.changeBackground &&
-                                                <LoadingWidget
-                                                    stopLoader={this.state.stopLoader}
-                                                />
-                                            }
+                                            <LoadingWidget
+                                                stopLoader={this.state.stopLoader}
+                                            />
                                         </div>
                                     }
                                 </Spring>
                             </div>
-                        </div> */}
-                        {/* <Spring
-                            from={{ backgroundColor: 'white', opacity: 1, zIndex: 1 }}
-                            to={{ opacity: !this.state.imagesLoaded ? 1 : 0, zIndex: !this.state.imagesLoaded ? 1 : -1 }}
-                            config={ config.mollases }
-                            >
-                            {props => 
-                                <div style={props} className='loading-screen'>
-                                    <LoadingWidget />
-                                </div>  
-                            }
-                        </Spring> */}
-                        {this.state.imagesLoaded &&
-                            <Navbar2 />
-                        }
-                        <div className='logo'>
-                            <p>Piros <br /> Photography.</p>
                         </div>
+                        {this.state.imagesLoaded &&
+                            <React.Fragment>
+                                <Navbar2 />
+                                <div className='logo'>
+                                    <p>Piros <br /> Photography.</p>
+                                </div>
+                            </React.Fragment>
+                        }
                         <div className='stories-page_title'>
-                            <h1 onClick={this.test}>Stories</h1>
-                            <h2>Humans and the world told through my camera</h2>
+                            <Spring
+                                from={{ transform: 'translateY(200px)' }}
+                                to={{ transform: this.state.titlesIn ? 'translateY(0px)' : 'translateY(100px)' }}
+                                config={config.slow}
+                            >
+                                {props =>
+                                    <div style={props} className='stories-page_title_main'>
+                                        <h1 onClick={this.test}>Stories</h1>
+                                    </div>
+                                }
+                            </Spring>
+                            <Spring
+                                from={{ transform: 'translateY(200px)' }}
+                                to={{ transform: this.state.titlesIn ? 'translateY(0px)' : 'translateY(100px)' }}
+                                config={config.slow}
+                            >
+                                {props =>
+                                    <div className='stories-page_title_sub'>
+                                        <h2 style={props}>Humans and the world told through my camera</h2>
+                                    </div>
+                                }
+                            </Spring>
                         </div>
                         <div onClick={this.test} className='stories-page_story_container'>
                             {this.state.stories.map((story, i) =>
-                                <div className={`stories-page_story stories-page_story--${i}`} key={i}
-                                // style={this.state.containerStyles[i] ? this.state.containerStyles[i] : null}
-                                >
+                                <div className='stories-page_story' key={i}>
                                     <NavLink to={{
                                         pathname: `/stories/story`,
                                         state: {
@@ -322,17 +172,23 @@ class StoriesX extends Component {
                                             mainImage: this.state.stories.map(story => story.mainImageURL)
                                         }
                                     }}>
-                                        <div className={'stories-page_story_img-container'}
-                                        // style={this.state.imageStyles[i] ? this.state.imageStyles[i] : null}
+                                        <Spring
+                                            from={{ transform: 'translateY(600px)' }}
+                                            to={{ transform: this.state.titlesIn ? 'translateY(0%)' : 'translateY(600px)' }}
+                                            config={config.slow}
                                         >
-                                            <div className={`stories-page_story_title`}>
-                                                <h3>{story.title}</h3>
-                                            </div>
-                                            <img onLoad={this.imagesOnLoad} className={`stories-page_story_image stories-page_story_image--${i}`} src={`${story.mainImageURL}`} value={story.key} alt=''
-                                                onClick={this.showStory}
-                                            >
-                                            </img>
-                                        </div>
+                                            {props =>
+                                                <div style={props} className='stories-page_story_img-container'>
+                                                    <div className={`stories-page_story_title`}>
+                                                        <h3>{story.title}</h3>
+                                                    </div>
+                                                    <img onLoad={this.imagesOnLoad} className='stories-page_story_image' src={`${story.mainImageURL}`} value={story.key} alt=''
+                                                        onClick={this.showStory}
+                                                    >
+                                                    </img>
+                                                </div>
+                                            }
+                                        </Spring>
                                     </NavLink>
                                 </div>
                             )}
