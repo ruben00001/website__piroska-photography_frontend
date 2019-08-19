d6e256f76b38e29b8e38d870af8814b8eb942f8d
unknown <a.ruben00001@gmail.com>
2019-08-19 14:49:35 +0100

    zoom arrows move to bottom

--- src/jsx/components/Zoom.jsx
+++ src/jsx/components/Zoom.jsx
@@ -6,7 +6,7 @@ import { faTimes, faChevronRight, faChevronLeft } from "@fortawesome/free-solid-
 const Zoom = (props) => {
   return (
     <Spring
-      from={{ opacity: 0, transform: 'scale(0.95)' }}
+      from={{ opacity: 0.6, transform: 'scale(0.95)' }}
       to={{
         opacity: 1,
         transform: 'scale(1)'
@@ -16,21 +16,23 @@ const Zoom = (props) => {
         <div style={propsA} className='zoom'>
           <div className='zoom_image'>
             <img src={props.zoomedImageURL} alt=''></img>
-            <FontAwesomeIcon onClick={props.previousPicture} className='zoom_arrow zoom_arrow--left' icon={faChevronLeft}></FontAwesomeIcon>
-            <FontAwesomeIcon onClick={props.nextPicture} className='zoom_arrow zoom_arrow--right' icon={faChevronRight}></FontAwesomeIcon>
             <FontAwesomeIcon onClick={props.exitZoom} className='zoom_x' icon={faTimes}></FontAwesomeIcon>
             <div className='zoom_info'>
-              <div className="zoom_info_counter">
-                <div className="zoom_info_counter_number">{props.pictureNum}.</div>
-                <Spring
-                  from={{}}
-                  to={{ background: `linear-gradient(to right, white, white ${props.pgnationBG}%, #3f3f3f ${props.pgnationBG}%, #3f3f3f 100%)` }}
-                >
-                  {propsB => (
-                    <div style={propsB} className="zoom_info_counter_line"></div>
-                  )}
-                </Spring>
-                <div className="zoom_info_counter_number">{props.numImages}.</div>
+              <div className='zoom_info_control'>
+                <FontAwesomeIcon onClick={props.previousPicture} className='zoom_arrow zoom_arrow--left' icon={faChevronLeft}></FontAwesomeIcon>
+                <div className="zoom_info_counter">
+                  <div className="zoom_info_counter_number">{props.pictureNum}.</div>
+                  <Spring
+                    from={{}}
+                    to={{ background: `linear-gradient(to right, white, white ${props.pgnationBG}%, #3f3f3f ${props.pgnationBG}%, #3f3f3f 100%)` }}
+                  >
+                    {propsB => (
+                      <div style={propsB} className="zoom_info_counter_line"></div>
+                    )}
+                  </Spring>
+                  <div className="zoom_info_counter_number">{props.numImages}.</div>
+                </div>
+                <FontAwesomeIcon onClick={props.nextPicture} className='zoom_arrow zoom_arrow--right' icon={faChevronRight}></FontAwesomeIcon>
               </div>
               <div className='zoom_info_images-title'>{props.imagesName}</div>
             </div>
